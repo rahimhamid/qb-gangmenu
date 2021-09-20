5e954a2b927b69c186b3baac5865e9df17137a22
rahimhamid <78023684+rahimhamid@users.noreply.github.com>
2021-09-20 19:17:24 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,10 +1,10 @@
 Config = {}
 
 Config.Gangs = {
-    ['lostmc'] = vector3(0, 0, 0),
-    ['ballas'] = vector3(86.75, -1958.28, 21.12),
+    ['onlytrustfamily'] = vector3(-1876.24, 2060.97, 145.57),
+    ['montanacartel'] = vector3(1395.13, 1160.11, 114.33),
     ['vagos'] = vector3(0, 0, 0),
     ['cartel'] = vector3(0, 0, 0),
-    ['families'] = vector3(0, 0, 0),
+    ['lafamilia'] = vector3(-318.79, -1351.85, 18.28),
     ['triads'] = vector3(0, 0, 0)
 }
